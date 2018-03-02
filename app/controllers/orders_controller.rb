89a66e4faf711e72f99f91d59c954303614756ed
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-03-02 14:01:52 +0000

    Added order details to order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,10 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
-      redirect_to order, notice: 'Your Order has been placed.'
+      flash[:success] = 'Your order has been placed. Thank you!'
+      redirect_to order
     else
-      redirect_to cart_path, flash: { error: order.errors.full_messages.first }
+      redirect_to cart_path, flash[:danger] = { error: order.errors.full_messages.first }
     end
 
   rescue Stripe::CardError => e
@@ -30,7 +31,7 @@ class OrdersController < ApplicationController
     Stripe::Charge.create(
       source:      params[:stripeToken],
       amount:      cart_total, # in cents
-      description: "Khurram Virani's Jungle Order",
+      description: "Your Jungle Order",
       currency:    'cad'
     )
   end
