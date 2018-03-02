3001c914637f12d0776da7a0f8c51f43c10adfa8
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-03-02 01:37:41 +0000

    Fully functional login/logout/register

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get '/register' => 'users#new'
   post '/register' => 'users#create'
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
